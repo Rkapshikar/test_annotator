1c219b60c92ae6591944df6ea784b1f4ac82f322
Revati Kapshikar <rkapshikar@berkeley.edu>
2015-10-10 02:34:45 -0700

    modified

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# test_annotator
+# SVIChallenge
+Website for submissions for SVIC
+
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+
---
+++ scripts/default.js
@@ -0,0 +1,14 @@
+function onLoad(){
+ window.fitText(document.getElementById("title"),2.2);
+}
+
+function loadChallenge(c) {
+     alert(c);    
+      window.location.href='the_link_to_go_to.html';
+          // The function returns the product of p1 and p2
+ }
+
+
+
+
+
